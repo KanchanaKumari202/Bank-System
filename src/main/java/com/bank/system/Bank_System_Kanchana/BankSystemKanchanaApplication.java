23e3525235c6052a65375294fa3479a8c942bf91
Kanchana <kanchana@cemexhrm.com>
2024-10-25 00:12:41 +0300

    Initial commit - DB connection established

---
+++ src/main/java/com/bank/system/Bank_System_Kanchana/BankSystemKanchanaApplication.java
@@ -0,0 +1,13 @@
+package com.bank.system.Bank_System_Kanchana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSystemKanchanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSystemKanchanaApplication.class, args);
+	}
+
+}

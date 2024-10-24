97a167095f118c71370ab7eea68b5e61b1054b00
Kanchana <kanchana@cemexhrm.com>
2024-10-25 00:22:28 +0300

    Initial commit - DB connection established

---
+++ src/test/java/com/bank/system/Bank_System_Kanchana/BankSystemKanchanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.system.Bank_System_Kanchana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSystemKanchanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
